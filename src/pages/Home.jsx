9f46ca24bc2a3e44def22e511aafabf346eb1211
Dhruvil099 <shahdhruvil1310@gmail.com>
2025-03-29 18:32:56 +0530

    Vedant Vite+React template

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <Link to="/register">Register</Link>
+      {/* <Link to="/register">Register</Link>
       <br></br>
-      <Link to="/login">Login</Link>
+      <Link to="/login">Login</Link> */}
+      Hello world
     </div>
   );
 }
